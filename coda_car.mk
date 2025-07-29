e6424f1372252b0287e182033cb98b90b9e4d016
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-07-29 20:29:38 +0300

    added makefile that sets evs config + rc scripts for static ip and hw initialization + added vsomeip/commonapi libs

---
+++ coda_car.mk
@@ -0,0 +1,16 @@
+PRODUCT_PACKAGES += libc++ libcommonapi libcommonapi_someip libvsomeip3 libvsomeip_cfg libvsomeip_e2e libvsomeip_sd libvsomeip android.hardware.automotive.evs-default 
+
+PRODUCT_COPY_FILES += \
+		vendor/Coda/static_ip/init.staticip.rc:root/init.staticip.rc \
+		vendor/Coda/init_hw/init.codahw.rc:root/init.codahw.rc \
+		vendor/Coda/evs_config/evs_configuration_override.xml:$(TARGET_COPY_OUT_VENDOR)/etc/automotive/evs/evs_configuration_override.xml \
+		
+#PRODUCT_PACKAGES += \
+#	VoiceInteraction 
+
+#         vendor/Avalon/init.driverlauncher.rc:root/init.driverlauncher.rc \
+# MultiDisplaySecondaryHomeTestLauncher DriverLauncher
+
+# PRODUCT_COPY_FILES += vendor/Avalon/init.avalonhw.rc:root/init.avalonhw.rc vendor/Avalon/MUMD/InputPortAssociation/input-port-associations.xml:$(TARGET_COPY_OUT_VENDOR)/etc/input-port-associations.xml
+
+# BOARD_SEPOLICY_DIRS += vendor/Avalon/GPS/sepolicy 
